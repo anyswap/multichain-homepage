b380bb9425d5957d736517d15d2fe30c2f336322
d_winter <2624376436@qq.com>
2021-12-22 17:34:03 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ export default {
   github: 'https://github.com/anyswap',
   telegram: 'https://t.me/anyswap',
   medium: 'https://medium.com/@anyswap',
-  twitter: 'https://twitter.com/AnyswapNetwork',
+  twitter: 'https://twitter.com/MultichainOrg',
   coinmarketcap: 'https://coinmarketcap.com/currencies/anyswap/',
   coingecko: 'https://www.coingecko.com/en/coins/anyswap'
 }
